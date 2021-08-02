eee49534a296d054853d14a164acea5cf73beaed
iiRedDev <73763294+iiRedDev@users.noreply.github.com>
2021-08-02 19:15:52 +0200

    Add files via upload

---
+++ progressBars/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "adamant"
+games { "rdr3" }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+ui_page 'html/iiRed_progressBar.html'
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'html/iiRed_progressBar.html',
+    'html/css/style.css',
+    'html/js/cercle.js',
+    
+}
+
+export "startUI"
+--[[ Copyright © by iiRedDev - 2021 --]]
